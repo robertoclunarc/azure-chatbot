d385d7a04093647784fdd20931fd6a4b57a53069
robertoclunarc <robertoclunarg@gmail.com>
2023-11-26 12:23:29 -0400

    fix validacion story_mention 1.9

--- src/functions/httpTriggerChatBotAzure.js
+++ src/functions/httpTriggerChatBotAzure.js
@@ -41,9 +41,11 @@ async function validPostRequest(request, context) {
             idRecipient = data?.entry[0]?.messaging[0]?.sender?.id;
             console.log(`type: ${JSON.stringify(data?.entry[0]?.messaging[0]?.message?.attachments)}`);
             if (data?.entry[0]?.messaging[0]?.message?.attachments[0]?.type=='story_mention'){
+                console.log('entro en el if');
                 message = process.env.respuestamencion;
             }
             else{
+                console.log(process.env.respuestamencion);
                 message = data?.entry[0]?.messaging[0]?.message?.text;
             }
             
