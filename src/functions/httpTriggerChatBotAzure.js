3f2566e06ecbf72fffd2f8baa12939153edc6fe7
robertoclunarc <robertoclunarg@gmail.com>
2023-12-14 19:43:09 -0400

    add envio de mesj a fb 1.4

--- src/functions/httpTriggerChatBotAzure.js
+++ src/functions/httpTriggerChatBotAzure.js
@@ -9,7 +9,7 @@ app.http('httpTriggerChatBotAzure', {
             return handleGetRequest(request, context);
         } else {
             const requestValid = await validPostRequest(request, context);
-            context.log(requestValid);
+            //context.log(requestValid);
             if (requestValid?.status===500 || requestValid?.payload==="1" || requestValid?.idRecipient === process.env.serderIdVentaIntagram) {                
                 const respn = {
                     msj:"El bot no respondera...", 
@@ -287,7 +287,7 @@ async function guardarConversacion(url, role, message, dateTime, sender, object)
 
 async function sendMessageToFacebook(context, reply) {
     const PAGE_ACCESS_TOKEN = "EAACjOBRZAdokBAM9ZCIj3waCRzUi6gioYna9ax2NeGzUcilEpInrtEpO5ajkCB7JNZBeXxPoJyGqiTXQVrBQ0CPCVATsZAu9ELYVhPjJzCaxZBo5lV3pux5ldkSad8RC4ZAmFgd27sN6doVnqR7tjVKW4GuyHS693dnk7RjOgnF9FflQfDNBNa9hZAhcZALEBnQZD";
-    const LATEST_API_VERSION = "v2.6";
+    const LATEST_API_VERSION = "v16.0";
     
     const body = {
         recipient: { id: context.idRecipient },
@@ -297,8 +297,8 @@ async function sendMessageToFacebook(context, reply) {
     };
     
     const URLfacebook = `https://graph.facebook.com/${LATEST_API_VERSION}/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;    
-    console.log(URLInstagram);    
-    console.log(body);
+    console.log(`urlFacebook: ${URLfacebook}`);    
+    console.log(`bodyFAcebook: ${JSON.stringify(body)}`);
     try {
         const responseData = await axios.post(URLfacebook, JSON.stringify(body), {
             headers: {
